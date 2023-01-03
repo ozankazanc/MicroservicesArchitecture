29d4df35f329c827f6fa33e5a0f51ece78e772da
Ozan Kazanç <ozan.kazanc@hotmail.com>
2023-01-03 22:27:39 +0300

    Dtos oluşturuldu.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Models/Feature.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Models
+{
+    public class Feature
+    {
+        /// <summary>
+        /// Kurs süresi
+        /// </summary>
+        public int Duration { get; set; }
+    }
+}
