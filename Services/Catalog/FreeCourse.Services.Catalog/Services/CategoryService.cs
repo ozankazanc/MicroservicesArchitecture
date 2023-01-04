4b0ffcae4579e2e189d771e0573c14dd9312f246
Ozan Kazanç <ozan.kazanc@hotmail.com>
2023-01-04 22:59:15 +0300

    Controller'lar hazırlandı.

--- Services/Catalog/FreeCourse.Services.Catalog/Services/CategoryService.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/CategoryService.cs
@@ -43,8 +43,10 @@ namespace FreeCourse.Services.Catalog.Services
 
         }
 
-        public async Task<Response<CategoryDto>> CreateAsync(Category category)
+        public async Task<Response<CategoryDto>> CreateAsync(CategoryDto categoryDto)
         {
+            var category = _mapper.Map<Category>(categoryDto);
+
             await _categoryCollection.InsertOneAsync(category);
 
             return Response<CategoryDto>.Success(_mapper.Map<CategoryDto>(category), 200);
